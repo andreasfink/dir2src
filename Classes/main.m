28f01a975ed4853bdde4979ce458f04fb26e5ccb
Andreas Fink <andreas@fink.org>
2018-03-23 17:25:44 +0100

    checkin

---
+++ Classes/main.m
@@ -0,0 +1,342 @@
+//
+//  main.m
+//  dir2src
+//
+//  Created by Andreas Fink on 14.10.14.
+//  Copyright (c) 2014 SMSRelay AG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main2(int argc, const char * argv[]);
+void processFile(NSString *root, NSString *relativePath,int *count);
+NSString *mimeTypeForFilename(NSString *filename);
+
+#include <stddef.h>
+#include <stdio.h>
+#include <time.h>
+
+FILE *fhead  = NULL;
+FILE *fout   = NULL;
+const char *headfilename = "out.h";
+const char *outfilename = "out.m";
+const char *toolname = "dir2src";
+
+NSMutableArray *allFiles = NULL;
+
+void header(void);
+void write_files(void);
+void footer(void);
+
+void header(void)
+{
+    time_t now;
+    
+    time(&now);
+    const char *nowString = ctime(&now);
+    
+    fprintf(fhead,"//\n");
+    fprintf(fhead,"//\n");
+    fprintf(fhead,"//\n");
+    fprintf(fhead,"// automatically generated using '%s'\n",toolname);
+    fprintf(fhead,"// on %s",nowString);
+    fprintf(fhead,"\n");
+
+    fprintf(fout,"//\n");
+    fprintf(fout,"//\n");
+    fprintf(fout,"//\n");
+    fprintf(fout,"// automatically generated using '%s'\n",toolname);
+    fprintf(fout,"// on %s",nowString);
+    fprintf(fout,"\n");
+
+    fprintf(fhead,"\n");
+    fprintf(fhead,"#import <unistd.h>\n");
+    fprintf(fhead,"\n");
+    
+    fprintf(fhead,"typedef struct webDirEntry\n");
+    fprintf(fhead,"{\n");
+    fprintf(fhead,"    const char *path;\n");
+    fprintf(fhead,"    const char *mimeType;\n");
+    fprintf(fhead,"    const char *data;\n");
+    fprintf(fhead,"    size_t datalen;\n");
+    fprintf(fhead,"} webDirEntry;\n");
+    fprintf(fhead,"\n");
+    fprintf(fhead,"extern webDirEntry allWebDirEntries[];\n");
+    fprintf(fhead,"extern int allWebDirEntriesCount;\n");
+    fprintf(fhead,"\n");
+
+    fprintf(fout,"#include \"%s\"\n",headfilename);
+    
+    fprintf(fout,"\n");
+    fprintf(fout,"webDirEntry allWebDirEntries[] =\n");
+    fprintf(fout,"{\n");
+}
+
+void footer (void)
+{
+    int count = (int) [allFiles count];
+    fprintf(fout,"};\n\n");
+    fprintf(fout,"int allWebDirEntriesCount = %d;\n\n",count);
+    fprintf(fout,"\n");
+}
+
+@interface FileEntry : NSObject
+{
+    NSString *absolutePath;
+    NSString *relativePath;
+    NSString *webPath;
+    NSString *mimeType;
+    NSData   *data;
+}
+
+@property(strong)    NSString *absolutePath;
+@property(strong)    NSString *relativePath;
+@property(strong)    NSString *webPath;
+@property(strong)    NSString *mimeType;
+@property(strong)    NSData   *data;
+
+- (NSComparisonResult)caseInsensitiveCompare:(FileEntry *)other;
+
+@end
+
+@implementation FileEntry
+
+@synthesize absolutePath;
+@synthesize relativePath;
+@synthesize webPath;
+@synthesize mimeType;
+@synthesize data;
+
+- (NSComparisonResult)caseInsensitiveCompare:(FileEntry *)other
+{
+    return [webPath caseInsensitiveCompare:other.webPath];
+}
+
+@end
+
+
+int main(int argc, const char *argv[])
+{
+    @autoreleasepool
+	{
+		const char **argv2 = (void *)malloc(sizeof(char *) * argc);
+
+		toolname = argv[0];
+		int i;
+		int j=0;
+		argv2[j++]=argv[0];
+		if(argc==1)
+		{
+			fprintf(stderr,"Usage: %s --header <headerfile.h> --output <outputfile.m> directory1 directory2...\n",toolname);
+			exit(0);
+		}
+		for(i=1;i<argc;i++)
+		{
+			const char *option = argv[i];
+
+			if((strcmp(option,"-?")==0) || (strcmp(option,"-h")==0) || (strcmp(option,"--help")==0))
+			{
+				fprintf(stderr,"Usage: %s --header <headerfile.h> --output <outputfile.m> directory1 directory2...\n",toolname);
+				exit(0);
+			}
+			if(strcmp(option,"--header")==0)
+			{
+				i++;
+				if(i < argc)
+				{
+					headfilename = argv[i];
+				}
+			}
+			else if(strcmp(option,"--output")==0)
+			{
+				i++;
+				if(i < argc)
+				{
+					outfilename = argv[i];
+				}
+			}
+			else
+			{
+				argv2[j++]=argv[i];
+			}
+		}
+		int argc2 = j;
+		fprintf(stderr,"Writing to header %s\n",headfilename);
+		fhead  = fopen(headfilename,"w");
+		if(fhead ==NULL)
+		{
+			fprintf(stderr,"Error: can not write to '%s'\n",headfilename);
+			exit(-1);
+		}
+		fprintf(stderr,"Writing to outputfile %s\n",outfilename);
+		fout   = fopen(outfilename,"w");
+		if(fout ==NULL)
+		{
+			fprintf(stderr,"Error: can not write to '%s'\n",outfilename);
+			exit(-1);
+		}
+		main2(argc2,argv2);
+		fclose(fout);
+		fclose(fhead);
+	}
+}
+
+int main2(int argc, const char * argv[])
+{
+    int i;
+    int count = 0;
+    
+    
+    @autoreleasepool
+    {
+        allFiles = [[NSMutableArray alloc]init];
+        for(i=1;i<argc;i++)
+        {
+            NSFileManager *mgr = [NSFileManager defaultManager];
+            NSString *path = [NSString stringWithUTF8String:argv[i]];
+            fprintf(stderr,"Traversing directory %s\n",argv[i]);
+
+            
+            for (NSString *filePath in [mgr enumeratorAtPath:path]) {
+			    NSError *err = nil;
+			    NSDictionary *itemInfo = [mgr attributesOfItemAtPath:[path stringByAppendingPathComponent:filePath] error:&err];
+				if (itemInfo)
+			    {
+					if ([itemInfo objectForKey:NSFileTypeDirectory] == NSFileTypeDirectory)
+					{
+						fprintf(stderr,"Traversing directory %s\n",filePath.UTF8String);
+					}
+					else
+					{
+						if ([itemInfo objectForKey:NSFileType] == NSFileTypeRegular)
+						{
+							processFile(path, filePath,&count);
+						}
+					}
+				}
+				else
+				{
+				    fprintf(stderr, "Error getting attributes of %s: %s\n", filePath.UTF8String, err.localizedDescription.UTF8String);
+				}
+            }
+        }
+        header();
+        write_files();
+        footer();
+    }
+    return 0;
+}
+
+NSString *mimeTypeForFilename(NSString *filename)
+{
+    if(!filename)
+    {
+        return nil;
+    }
+    if([filename hasSuffix:@"txt"])
+    {
+        return @"text/plain; charset=\"UTF-8\"";
+    }
+    else if([filename hasSuffix:@"html"])
+    {
+        return @"text/html; charset=\"UTF-8\"";
+    }
+    else if([filename hasSuffix:@"css"])
+    {
+        return @"text/css";
+    }
+    else if([filename hasSuffix:@"png"])
+    {
+        return @"image/png";
+    }
+    else if([filename hasSuffix:@"jpg"])
+    {
+        return @"image/jpeg";
+    }
+    else if([filename hasSuffix:@"jpeg"])
+    {
+        return @"image/jpeg";
+    }
+    else if([filename hasSuffix:@"gif"])
+    {
+        return @"image/gif";
+    }
+    else if([filename hasSuffix:@"js"])
+    {
+        return @"application/javascript";
+    }
+    return @"application/octet-stream";
+}
+
+void processFile(NSString *root, NSString *relativePath,int *count)
+{
+    
+    /* we explicitly exclude files ending in .json as they are placeholder for dynamic data used while testing */
+
+    if([relativePath hasSuffix:@"json"])
+    {
+        return;
+    }
+    if([relativePath hasSuffix:@"DS_Store"])
+    {
+        return;
+    }
+    if([relativePath hasSuffix:@"git"])
+    {
+        return;
+    }
+    if([relativePath hasSuffix:@"svn"])
+    {
+        return;
+    }
+    NSLog(@"Processing file: %@",relativePath);
+    
+    
+    FileEntry *e = [[FileEntry alloc]init];
+
+    e.absolutePath = [NSString stringWithFormat:@"%@/%@",root,relativePath];
+    e.relativePath = relativePath;
+    e.webPath = [NSString stringWithFormat:@"/%@",relativePath];
+    e.mimeType = mimeTypeForFilename(relativePath);
+    e.data = [NSData dataWithContentsOfFile:e.absolutePath];
+    [allFiles addObject:e];
+}
+
+
+void write_files(void)
+{
+    NSArray *sortedFiles =  [allFiles sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+    
+    for(FileEntry *entry in sortedFiles)
+    {
+        uint8_t *ptr = (uint8_t *)[entry.data bytes];
+        size_t len = [entry.data length];
+        
+        int i;
+
+        fprintf(fout,"\t{\n");
+        fprintf(fout,"\t\t\"%s\",\n",entry.webPath.UTF8String);
+
+        if([entry.relativePath hasSuffix:@"txt"])
+        {
+            fprintf(fout,"\t\t\"text/plain; charset=\\\"UTF-8\\\"\",\n");
+        }
+        else if([entry.relativePath hasSuffix:@"html"])
+        {
+            fprintf(fout,"\t\t\"text/html; charset=\\\"UTF-8\\\"\",\n");
+        }
+        else
+        {
+            fprintf(fout,"\t\t\"%s\",\n",entry.mimeType.UTF8String);
+        }
+        fprintf(fout,"\t\t\"");
+        
+        for(i=0;i<len;i++)
+        {
+            fprintf(fout,"\\x%02X",ptr[i]);
+        }
+        fprintf(fout,"\",\n");
+        fprintf(fout,"\t\t%d\n",(int)len);
+        fprintf(fout,"\t},\n");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,84 @@
 # dir2src
 Convert directory with files to C code to include.
+
+
+Let's assume you have an integrated mini webserver in your application which dynamically creates HTML pages.
+However you have some statical content such as CSS files or so which you have to serve as well.
+You can have these files separate from the binary or you can embed them. This tool is for makign the second 
+case easier. It is written in ObjectiveC but outputs plain C code. Under Linux it uses the gnustep-base Foundation classes.
+
+
+
+Example
+
+you have a directory structure like tis:
+
+	web/
+		index.html
+		css/
+			style.css
+			
+you call:
+
+	dir2src --header web.h --output web.m web
+
+
+and you will end up with  web.h and web.m you can include in your C or ObjectiveC code to access this data.
+
+
+To initialize it this code snippet in ObjectiveC will create a dictionary with the pages you generated
+
+		#import "web.h"
+		
+		NSMutableDictionary *_webPages;
+		
+		void initStaticPages()
+		{
+			_webPages = [[NSMutableDictionary alloc]init];
+			int i=0;
+			int n = allWebDirEntriesCount;
+			for(i=0;i<n;i++)
+			{
+				webDirEntry *e = &allWebDirEntries[i];
+				NSMutableDictionary *entry = [[NSMutableDictionary alloc]init];
+				NSString *path =  @(e->path);
+				entry[@"path"] = path;
+				entry[@"content-type"] = @(e->mimeType);
+				entry[@"data"] = [NSData dataWithBytes:e->data length:e->datalen ];
+				_webPages[path] = entry;
+			}
+		}
+
+If you use the integrated webserver of ulib (http://github.com/andreasfink/ulib) you can serve these 
+built in pages like this:
+
+
+	- (void)  httpGetPost:(UMHTTPRequest *)req
+	{
+		@autoreleasepool
+		{
+			NSString *path = req.url.relativePath;
+			NSLog(@"WebPathRequested: %@",path);
+			if([path hasPrefix:@"/api"])
+			{
+				/* do your generated code here */
+				[req setResponseJsonObject:@{ @"error" : @"api-not-found" }];
+			}
+			else /* any non generated page */
+			{
+				NSDictionary *entry = _webPages[path];
+				if(entry)
+				{
+					[req setContentType:entry[@"content-type"]];
+					[req setResponseData:entry[@"data"]];
+				}
+				else
+				{
+					NSString *s = @"Result: Error\nReason: Unknown request\n";
+					[req setResponseTypeText];
+					req.responseData = [s dataUsingEncoding:NSUTF8StringEncoding];
+					req.responseCode =  404;
+				}
+			}
+		}
+	}
